543c72f8e75bebe0ec7938bb5352ab1c6290820c
cysazi <cyrus.shirazi@me.com>
2026-01-21 10:37:46 -0800

    couple changes to header stuff

--- ESP-32-BNO085-DataCollector/ESP-32-BNO085-DataCollector.ino
+++ ESP-32-BNO085-DataCollector/ESP-32-BNO085-DataCollector.ino
@@ -1,15 +1,16 @@
 // Jazz Hand (1/2) (RIGHT/LEFT) <- to be decided
 
-const uint8_t relayMAC[] = {}  // The MAC address of the relay ESP-32, constant for this device.
+const uint8_t relayMAC[] = { 1, 2, 3, 4, 5 };  // The MAC address of the relay ESP-32, constant for this device.
 
 struct __attribute__((__packed__)) SensorData {  // has some special syntax to tell arduino to leave the raw data in byte form
-  uint16_t header;                               // 2 bytes
-  uint_t device_id;                              // 1 byte
+  uint16_t header = 0xAAAA;                      // 2 bytes
+  uint8_t device_id;                             // 1 byte
+  uint32_t timestamp;                            // 4 bytes
   float accel_x, accel_y, accel_z;               // 4*3 = 12 bytes
-  float quat_W, quat_x, quat_y, quat_z;          // 4*4 = 16 bytes
-  float UWB_distance;                            // 4 bytes
+  float UWB_distance1, UWB_distance2;            // 4*2 = 8 bytes
   uint8_t button_state;                          // 1 byte
-};                                               // Total: 36 bytes
+  float quat_W, quat_x, quat_y, quat_z;          // 4*4 = 16 bytes
+};                                               // Total: 44 bytes
 
 void setup() {
   Serial.begin(115200);  // This project uses a higher baud rate than we did in BME 60A
@@ -18,7 +19,7 @@ void setup() {
 
 void loop() {
   SensorData current_readings = collectData();  // collect the data
-  sendPacket(current_readings)                  // send the data
+  sendPacket(current_readings);                 // send the data
 }
 
 SensorData collectData() {  // this fn returns the struct with all the data
@@ -26,7 +27,7 @@ SensorData collectData() {  // this fn returns the struct with all the data
 
   // The data collection stuff goes here
 
-  return data  // returning the instance of the data to the outside world
+  return data;  // returning the instance of the data to the outside world
 }
 
 void sendPacket(SensorData data) {  // sends the struct to relay ESP-32 via ESP-NOW
