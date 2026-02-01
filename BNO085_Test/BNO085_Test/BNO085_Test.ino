b78634b9d2a0ca0f3501104f606d01e3ef74bc89
cysazi <cyrus.shirazi@me.com>
2026-02-01 15:41:34 -0800

    Added the BNO085 Test file

---
+++ BNO085_Test/BNO085_Test/BNO085_Test.ino
@@ -0,0 +1,88 @@
+#include <Wire.h>
+#include <Adafruit_BNO08x.h>
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+Adafruit_BNO08x bno08x;
+sh2_SensorValue_t sensorValue;
+
+unsigned long time_var = 0;
+unsigned long delta_time = 0;
+
+void setReports() {
+  Serial.println("Setting desired reports");
+  if (!bno08x.enableReport(SH2_ROTATION_VECTOR, 10000)) {
+    Serial.println("Could not enable rotation vector");
+  }
+  if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION, 10000)) {
+    Serial.println("Could not enable linear acceleration");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  
+  Serial.println("BNO08x test");
+  
+  Wire.begin(SDA_PIN, SCL_PIN);
+  Wire.setClock(100000);  // 100kHz for stability
+  delay(100);
+  
+  if (!bno08x.begin_I2C(0x4A, &Wire)) {
+    Serial.println("Failed to find BNO08x chip");
+    while (1) { delay(10); }
+  }
+  
+  Serial.println("BNO08x Found!");
+  setReports();
+  Serial.println("Reading events...");
+}
+
+// Global variables to store latest sensor data
+float quat_w = 0, quat_i = 0, quat_j = 0, quat_k = 0;
+float accel_x = 0, accel_y = 0, accel_z = 0;
+
+void loop() {
+  if (bno08x.wasReset()) {
+    Serial.println("sensor was reset!");
+    setReports();
+  }
+  
+  // Read sensor data and store latest values
+  if (bno08x.getSensorEvent(&sensorValue)) {
+    
+    switch (sensorValue.sensorId) {
+      case SH2_ROTATION_VECTOR:
+        quat_w = sensorValue.un.RotationVector.real;
+        quat_i = sensorValue.un.RotationVector.i;
+        quat_j = sensorValue.un.RotationVector.j;
+        quat_k = sensorValue.un.RotationVector.k;
+        break;
+        
+      case SH2_LINEAR_ACCELERATION:
+        accel_x = sensorValue.un.linearAcceleration.x;
+        accel_y = sensorValue.un.linearAcceleration.y;
+        accel_z = sensorValue.un.linearAcceleration.z;
+        break;
+      
+    }
+  }
+  
+  // Print both together every 10ms
+  if (millis() - delta_time > 10) {
+    delta_time = millis();
+    
+    Serial.print("Quat[");
+    Serial.print(quat_w, 3); Serial.print(",");
+    Serial.print(quat_i, 3); Serial.print(",");
+    Serial.print(quat_j, 3); Serial.print(",");
+    Serial.print(quat_k, 3);
+    Serial.print("] Accel[");
+    Serial.print(accel_x, 2); Serial.print(",");
+    Serial.print(accel_y, 2); Serial.print(",");
+    Serial.print(accel_z, 2);
+    Serial.println("]");
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Jazz-Hands
 E-SONIC glove-based musical instrument
+___
+# Important Info!
+## Arduino IDE setup guide
+### Board Setting
+The esp32 board library has examples for ESP_NOW (both broadcast master and slave)
+* Go to boards manager and download `esp32` by `Expressif Systems`
+  * Set your board to `ESP 32 Dev Module` (IMPORTANT, found out the hard way that other esp32 modules won't work the same)
+### Component Libraries
+All the libraries have great examples!
+#### BNO085
+* Download `Adafruit BNO08x` by `Adafruit` 
+* I think all you need to include are` <Wire.h>` and `<Adafruit_BNO08x.h>`
+#### UWB (DW1000)
+* Library is a .zip library found here: [Makerfabs GitHub]([https://github.com/Makerfabs/Makerfabs-ESP32-UWB/blob/main/mf_DW1000.zip])
+* Download `mf_DW10000.zip` then include it in Arduino IDE using `Sketch > Include Library > Add .ZIP Library`
+## ESP 32 Info
+### Mac Addresses
+| # | ESP32 Serial Address (MacOS)   | ESP-NOW Mac Address | ESP32 Serial Address (Win) | 
+|---|--------------------------------|---------------------|----------------------------|
+| 1 | /dev/cu.usbserial-023B6AB4     | 34:98:7A:74:39:00   | yall figure this one out   |
+| 2 | /dev/cu.usbserial-023B6B01     | 34:98:7A:73:75:B8   | cuz im not gonna use Win11 |
+| 3 | /dev/cu.usbserial-023B6AC7     | 34:98:7A:73:93:14   | for a while                |
+| 4 | haven't done this one yet lol  |                     |                            |
+
+
